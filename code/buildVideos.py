31fc7bbdd381a4fd4f360842f293434678cf3e64
philipp-mueller <pmueller@mpi-inf.mpg.de>
2018-02-17 16:04:23 +0100

    video writing

--- code/buildVideos.py
+++ code/buildVideos.py
@@ -15,21 +15,35 @@ out_height = 480
 
 
 def addBlackPadding(out,secs=2):
-    blackFrame = np.zeros((out_width,out_height,3))
-    for i in range(secs*fps):
+    blackFrame = np.zeros((out_height*2,out_width,3),dtype='uint8')
+    for i in range(int(secs*fps)):
         out.write(blackFrame)
-    
+    return out
+   
+def addStaticImg(out,imPath,secs=2):
+    img = cv2.imread(imPath)
+    # resize image to dimensions of video
+#    img = np.swapaxes(img,0,1)
+    size_multiplier = float(out_width)/img.shape[1]
+    new_height = int(img.shape[0]*size_multiplier)
+    img = cv2.resize(img,(out_width,new_height))
+    lowerPadding = np.zeros((2*out_height-new_height,out_width,3),dtype='uint8')
+    img = np.concatenate([img,lowerPadding],axis=0)
+    for i in range(int(secs*fps)):
+        out.write(img)
+    return out
+
 
 
 # open out video files and add initial black padding
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out_videoFiles = {}
+videoWriters = {}
 for raspi_id in raspi_ids:
-    curr_outPath = outDir+'raspi'+str(raspi_id)+'.avi'
+    out_path = outDir+'raspi'+str(raspi_id)+'.avi'
     out = cv2.VideoWriter(out_path,
                         fourcc,fps,(out_width,out_height*2))
-    addBlackPadding(out)
-    out_videoFiles[raspi_id] = out
+    out = addBlackPadding(out)
+    videoWriters[raspi_id] = out
 
 
 
@@ -39,13 +53,23 @@ for raspi_id in raspi_ids:
 # --------------------------------------------
 
 for raspi_id in raspi_ids:
-   
+    # get video writer for raspi_id
+    out = videoWriters[raspi_id]
+    # first add image forscene 1.4
+    imPath = imgInDir+'14_'+str(raspi_id)+'.jpg'
+    out = addStaticImg(out,imPath)
+    # then for scene 1.6
+    imPath = imgInDir+'16_'+str(raspi_id)+'.jpg'
+    out = addStaticImg(out,imPath)
+    out = addBlackPadding(out)
+    videoWriters[raspi_id] = out
 
-# resize imagfe with opencv:
+
+# to resize image with opencv:
 #resized_image = cv2.resize(image, (100, 50)) 
 
 
 
 # finally, close all video out files
 for raspi_id in raspi_ids:
-    out_videoFiles[raspi_id].release()
+    videoWriters[raspi_id].release()
